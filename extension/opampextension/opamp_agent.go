f0a534ed743e56e4332f4ef44fa9d549ff42d487
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2025-07-01 15:27:04 +0300

    opampextension: hardcode in-memory package store
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- extension/opampextension/opamp_agent.go
+++ extension/opampextension/opamp_agent.go
@@ -8,6 +8,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"runtime"
@@ -135,11 +136,12 @@ func (o *opampAgent) Start(ctx context.Context, host component.Host) error {
 	}
 
 	settings := types.StartSettings{
-		Header:         header,
-		HeaderFunc:     headerFunc,
-		TLSConfig:      tls,
-		OpAMPServerURL: o.cfg.Server.GetEndpoint(),
-		InstanceUid:    types.InstanceUid(o.instanceID),
+		PackagesStateProvider: NewInMemPackagesStore(),
+		Header:                header,
+		HeaderFunc:            headerFunc,
+		TLSConfig:             tls,
+		OpAMPServerURL:        o.cfg.Server.GetEndpoint(),
+		InstanceUid:           types.InstanceUid(o.instanceID),
 		Callbacks: types.Callbacks{
 			OnConnect: func(_ context.Context) {
 				o.logger.Debug("Connected to the OpAMP server")
@@ -694,4 +696,104 @@ func convertComponentHealth(statusUpdate *status.AggregateStatus) *protobufs.Com
 	}
 
 	return componentHealth
+}
+
+// InMemPackagesStore is a package store used for testing. Keeps the packages in memory.
+type InMemPackagesStore struct {
+	allPackagesHash      []byte
+	pkgState             map[string]types.PackageState
+	fileContents         map[string][]byte
+	fileSignatures       map[string][]byte
+	fileHashes           map[string][]byte
+	lastReportedStatuses *protobufs.PackageStatuses
+
+	onAllPackagesHash func()
+}
+
+var _ types.PackagesStateProvider = (*InMemPackagesStore)(nil)
+
+func NewInMemPackagesStore() *InMemPackagesStore {
+	return &InMemPackagesStore{
+		fileContents:   map[string][]byte{},
+		fileSignatures: map[string][]byte{},
+		fileHashes:     map[string][]byte{},
+		pkgState:       map[string]types.PackageState{},
+	}
+}
+
+func (l *InMemPackagesStore) AllPackagesHash() ([]byte, error) {
+	if l.onAllPackagesHash != nil {
+		l.onAllPackagesHash()
+	}
+	return l.allPackagesHash, nil
+}
+
+func (l *InMemPackagesStore) Packages() ([]string, error) {
+	var names []string
+	for k := range l.pkgState {
+		names = append(names, k)
+	}
+	return names, nil
+}
+
+func (l *InMemPackagesStore) PackageState(packageName string) (state types.PackageState, err error) {
+	if pkg, ok := l.pkgState[packageName]; ok {
+		return pkg, nil
+	}
+	return types.PackageState{Exists: false}, nil
+}
+
+func (l *InMemPackagesStore) CreatePackage(packageName string, typ protobufs.PackageType) error {
+	l.pkgState[packageName] = types.PackageState{
+		Exists: true,
+		Type:   typ,
+	}
+	return nil
+}
+
+func (l *InMemPackagesStore) FileContentHash(packageName string) ([]byte, error) {
+	return l.fileHashes[packageName], nil
+}
+
+func (l *InMemPackagesStore) UpdateContent(_ context.Context, packageName string, data io.Reader, contentHash, signature []byte) error {
+	b, err := io.ReadAll(data)
+	if err != nil {
+		return err
+	}
+	l.fileContents[packageName] = b
+	l.fileSignatures[packageName] = signature
+	l.fileHashes[packageName] = contentHash
+	return nil
+}
+
+func (l *InMemPackagesStore) SetPackageState(packageName string, state types.PackageState) error {
+	l.pkgState[packageName] = state
+	return nil
+}
+
+func (l *InMemPackagesStore) DeletePackage(packageName string) error {
+	delete(l.pkgState, packageName)
+	return nil
+}
+
+func (l *InMemPackagesStore) SetAllPackagesHash(hash []byte) error {
+	l.allPackagesHash = hash
+	return nil
+}
+
+func (l *InMemPackagesStore) GetContent() map[string][]byte {
+	return l.fileContents
+}
+
+func (l *InMemPackagesStore) GetSignature() map[string][]byte {
+	return l.fileSignatures
+}
+
+func (l *InMemPackagesStore) LastReportedStatuses() (*protobufs.PackageStatuses, error) {
+	return l.lastReportedStatuses, nil
+}
+
+func (l *InMemPackagesStore) SetLastReportedStatuses(statuses *protobufs.PackageStatuses) error {
+	l.lastReportedStatuses = statuses
+	return nil
 }
